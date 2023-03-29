53468d23f48aa6e36820bf1e0955f16214ce4a74
YutakaWatanabe55 <82863512+YutakaWatanabe55@users.noreply.github.com>
2023-03-29 18:56:48 +0900

    h

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 window.onload = () => {
-const backgrounds = [
-  'https://github.com/YutakaWatanabe55/ShirayamiWeb/raw/main/img/Tokyo_tower.jpg',
-  'https://github.com/YutakaWatanabe55/ShirayamiWeb/raw/main/img/shimanami.jpg',
-  'https://github.com/YutakaWatanabe55/ShirayamiWeb/raw/main/img/future_1.png'
-];
+  const backgrounds = [
+    'https://github.com/YutakaWatanabe55/ShirayamiWeb/raw/main/img/Tokyo_tower.jpg',
+    'https://github.com/YutakaWatanabe55/ShirayamiWeb/raw/main/img/shimanami.jpg',
+    'https://github.com/YutakaWatanabe55/ShirayamiWeb/raw/main/img/future_1.png'
+  ];
 
-const backgroundImages = document.getElementById('background-images');
-let currentBackground = 0;
+  const backgroundImages = document.getElementById('background-images');
+  let currentBackground = 0;
 
-setInterval(() => {
-  currentBackground = (currentBackground + 1) % backgrounds.length;
-  backgroundImages.style.backgroundImage = `url(${backgrounds[currentBackground]})`;
-}, 5000);
+  setInterval(() => {
+    currentBackground = (currentBackground + 1) % backgrounds.length;
+    backgroundImages.style.backgroundImage = `url(${backgrounds[currentBackground]})`;
+  }, 5000);
+
   backgroundImages.style.backgroundSize = 'cover';
   backgroundImages.style.backgroundPosition = 'center center';
 };
