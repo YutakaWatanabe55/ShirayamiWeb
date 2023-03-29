81f7c246313830983c4a81cf95d2d29c4b9572d3
YutakaWatanabe55 <82863512+YutakaWatanabe55@users.noreply.github.com>
2023-03-29 22:31:15 +0900

    cp

--- script.js
+++ script.js
@@ -17,32 +17,9 @@ window.onload = () => {
   backgroundImages.style.backgroundPosition = 'center center';
 
 
-  var slideIndex = 1;
-showSlides(slideIndex);
+ // var slideIndex = 1;
+//showSlides(slideIndex);
 
 };
 
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (!slides || slides.length === 0) return;
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
 
